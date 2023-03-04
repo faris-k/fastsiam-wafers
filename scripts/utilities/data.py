2ab50d96e226962569b99bafbcd0f3372bc4b6a2
Faris Khan <khanmohammedfaris@gmail.com>
2023-03-04 01:41:14 -0700

    Fix DPWTransform
    Fixed to work with torch tensors by default instead of numpy arrays (matching DieNoise behavior). Added to get_base_transforms, currently 50/50 noise/DPW

--- scripts/utilities/data.py
+++ scripts/utilities/data.py
@@ -40,6 +40,250 @@ class DieNoise:
         return sample
 
 
+# Inspired by Albumentations OneOf
+# https://albumentations.ai/docs/api_reference/core/composition/#albumentations.core.composition.OneOf
+# https://github.com/albumentations-team/albumentations/blob/master/albumentations/core/composition.py#L302
+class RandomOneOf:
+    """Randomly applies one of the given transforms with a given probability.
+
+    Parameters
+    ----------
+    transforms : List[torch.nn.Module]
+        List of transforms to apply.
+    weights : List[float], optional
+        List of weights for each transform. If None, all transforms are
+        equally likely to be applied. By default None.
+    p : float, optional
+        Probability of applying the RandomOneOf block at all, by default 1.0.
+    """
+
+    def __init__(
+        self,
+        transforms,
+        weights: List[float] = None,
+        p: float = 1.0,
+    ):
+
+        if weights:
+            if len(transforms) != len(weights):
+                raise ValueError(
+                    "The number of weights must match the number of transforms"
+                )
+            if not all([w >= 0 for w in weights]):
+                raise ValueError("Weights must be non-negative")
+            total_weight = sum(weights)
+            if total_weight == 0:
+                raise ValueError("At least one weight must be greater than 0")
+            self.weights = [w / total_weight for w in weights]
+        else:
+            self.weights = [1.0 / len(transforms)] * len(transforms)
+
+        if p is not None and (p < 0 or p > 1):
+            raise ValueError("p must be a float between 0 and 1")
+
+        self.transforms = transforms
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            idx = random.choices(range(len(self.transforms)), weights=self.weights)[0]
+            img = self.transforms[idx](img)
+
+        return img
+
+
+class DPWTransform:
+    """
+    Transforms a wafer map to a lower DPW version. Scale param is first
+    generated using a power law distribution, then randomization is applied
+    by using it as the lower bound of a beta distribution (skewed towards the lower bound).
+
+    Parameters
+    ----------
+    domain_lower : int, optional
+        Lower bound of shapeMaxDim in the dataset, by default 26
+    domain_upper : int, optional
+        Upper bound of shapeMaxDim in the dataset, by default 212
+    out_lower : float, optional
+        Lower bound of scale param, by default 0.4
+    out_upper : float, optional
+        Upper bound of scale param, by default 0.95
+    alpha : float, optional
+        Alpha param for beta distribution, by default 0.5
+    beta : float, optional
+        Beta param for beta distribution, by default 1.0
+    p : float, optional
+        Power param for power law distribution, by default 5.0
+    """
+
+    def __init__(
+        self,
+        domain_lower: int = 26,
+        domain_upper: int = 212,
+        out_lower: float = 0.4,
+        out_upper: float = 0.95,
+        alpha: float = 0.5,
+        beta: float = 1.5,
+        p: float = 5.0,
+    ):
+
+        self.domain_lower = domain_lower
+        self.domain_upper = domain_upper
+        self.out_lower = out_lower
+        self.out_upper = out_upper
+        self.alpha = alpha
+        self.beta = beta
+        self.p = p
+
+    def power_law_transform(
+        x,
+        domain_lower,
+        domain_upper,
+        out_lower=0.4,
+        out_upper=0.95,
+        p=5,
+    ):
+        # Handle edge cases
+        if x <= domain_lower:
+            return out_upper
+        if x >= domain_upper:
+            return out_lower
+
+        # Invert input domain
+        domain_range = domain_upper - domain_lower
+        inverted_x = abs(x - domain_lower)
+        normalized_x = inverted_x / domain_range
+        # Apply power law transformation
+        y = (1 - normalized_x) ** p
+        # Map result to output range
+        out_range = out_upper - out_lower
+        return out_lower + y * out_range
+
+    def generate_skewed_random(lower_bound, upper_bound=0.95, alpha=0.5, beta=1):
+        # Generate random number using beta distribution
+        x = np.random.beta(alpha, beta)
+        # Scale to range [lower_bound, upper_bound]
+        return lower_bound + (upper_bound - lower_bound) * x
+
+    # def dpw_transform(
+    #     wafermap,
+    #     scale: float,
+    # ):
+    #     """
+    #     Transforms a wafer map to a lower DPW version.
+    #     Works by taking the central coordinates of passing and failing die, then
+    #     mapping those coordinates to the new wafer map.
+
+    #     Parameters
+    #     ----------
+    #     wafermap : np.ndarray
+    #         Wafermap to transform
+    #     scale : float, optional
+    #         Scale to transform the wafer map to.
+    #         Must be between 0 and 1.
+    #     """
+    #     assert 0.0 < scale <= 1.0, "Scale must be between 0 and 1."
+
+    #     # Calculate the new dimensions of the wafer after scaling down
+    #     h, w = wafermap.shape
+    #     new_h = int(h * scale)
+    #     new_w = int(w * scale)
+    #     new_dim = (new_h, new_w)
+
+    #     # Find the indices of the passing elements in the original wafer
+    #     passing_indices = np.argwhere(wafermap == 128)
+
+    #     # Find the indices of the failing elements in the original wafer
+    #     failing_indices = np.argwhere(wafermap == 255)
+
+    #     # Calculate the relative central coordinate of the passing and failing elements in the original wafer
+    #     pass_coords = (passing_indices + 0.5) / wafermap.shape
+    #     fail_coords = (failing_indices + 0.5) / wafermap.shape
+
+    #     # Calculate the central coordinates of the passing and failing elements in the new wafer map
+    #     new_pass_coords = (pass_coords * new_dim).astype(int)
+    #     new_fail_coords = (fail_coords * new_dim).astype(int)
+
+    #     # Create the (new_h, new_w) wafer map
+    #     new_wafer = np.zeros(new_dim, dtype=int)
+
+    #     # Assign the passing elements in the new wafer map
+    #     new_wafer[new_pass_coords[:, 0], new_pass_coords[:, 1]] = 128
+
+    #     # Assign the failing elements in the new wafer map
+    #     new_wafer[new_fail_coords[:, 0], new_fail_coords[:, 1]] = 255
+
+    #     return new_wafer
+
+    def dpw_transform(wafermap: torch.Tensor, scale: float) -> torch.Tensor:
+        """
+        Transforms a wafer map to a lower DPW version.
+        Works by taking the central coordinates of passing and failing die, then
+        mapping those coordinates to the new wafer map.
+
+        Parameters
+        ----------
+        wafermap : torch.Tensor
+            Wafermap to transform
+        scale : float, optional
+            Scale to transform the wafer map to.
+            Must be between 0 and 1.
+        """
+
+        assert 0.0 < scale <= 1.0, "Scale must be between 0 and 1."
+
+        # Calculate the new dimensions of the wafer after scaling down
+        h, w = wafermap.shape
+        new_h = int(h * scale)
+        new_w = int(w * scale)
+
+        # Find the indices of the passing elements in the original wafer
+        passing_indices = torch.argwhere(wafermap == 128)  # (wafermap == 128).nonzero()
+
+        # Find the indices of the failing elements in the original wafer
+        failing_indices = torch.argwhere(wafermap == 255)  # (wafermap == 255).nonzero()
+
+        # Calculate the relative central coordinate of the passing and failing elements in the original wafer
+        pass_coords = (passing_indices + 0.5) / torch.tensor(wafermap.shape)
+        fail_coords = (failing_indices + 0.5) / torch.tensor(wafermap.shape)
+
+        # Calculate the central coordinates of the passing and failing elements in the new wafer map
+        new_pass_coords = (pass_coords * torch.tensor([new_h, new_w])).long()
+        new_fail_coords = (fail_coords * torch.tensor([new_h, new_w])).long()
+
+        # Create a tensor for storing transformed data with shape (new_h,new_w)
+        new_wafer = torch.zeros((new_h, new_w))
+
+        # Set values for passing and failing elements in transformed tensor
+        new_wafer[new_pass_coords[:, 0], new_pass_coords[:, 1]] = 128
+        new_wafer[new_fail_coords[:, 0], new_fail_coords[:, 1]] = 255
+
+        return new_wafer
+
+    def __call__(self, img):
+        # Calculate initial scale parameter using power_law_transform
+        max_dim = max(img.shape)
+        scale_init = DPWTransform.power_law_transform(
+            max_dim,
+            self.domain_lower,
+            self.domain_upper,
+            self.out_lower,
+            self.out_upper,
+            self.p,
+        )
+
+        # Generate a skewed random scale parameter
+        lower_bound = scale_init
+        scale = DPWTransform.generate_skewed_random(
+            lower_bound, self.out_upper, alpha=self.alpha, beta=self.beta
+        )
+
+        # Apply the DPW transform using the scale parameter
+        new_img = DPWTransform.dpw_transform(img, scale=scale)
+
+        return new_img
+
+
 norm_dir = os.path.join(os.getcwd(), "utilities", "normalization_stats.pth")
 normalization_stats = torch.load(norm_dir)
 
@@ -77,8 +321,15 @@ def get_base_transforms(
     """
 
     transforms = [
-        # Add die noise before anything else
-        DieNoise(die_noise_prob),
+        # Randomly perform either DieNoise or DPWTransform with a 50% chance
+        RandomOneOf(
+            [
+                DieNoise(die_noise_prob),
+                DPWTransform(),
+            ]
+        ),
+        # # Add die noise before anything else
+        # DieNoise(die_noise_prob),
         # Convert to PIL Image, then perform all torchvision transforms except cropping
         T.ToPILImage(),
         T.Resize(img_size, interpolation=InterpolationMode.NEAREST),
@@ -594,205 +845,6 @@ def get_inference_transforms(img_size: List[int] = [224, 224], normalize: bool =
         transforms.append(T.Normalize(*normalization_stats))
 
     return T.Compose(transforms)
-
-
-# Inspired by Albumentations OneOf
-# https://albumentations.ai/docs/api_reference/core/composition/#albumentations.core.composition.OneOf
-# https://github.com/albumentations-team/albumentations/blob/master/albumentations/core/composition.py#L302
-class RandomOneOf:
-    """Randomly applies one of the given transforms with a given probability.
-
-    Parameters
-    ----------
-    transforms : List[torch.nn.Module]
-        List of transforms to apply.
-    weights : List[float], optional
-        List of weights for each transform. If None, all transforms are
-        equally likely to be applied. By default None.
-    p : float, optional
-        Probability of applying the RandomOneOf block at all, by default 1.0.
-    """
-
-    def __init__(
-        self,
-        transforms: List[torch.nn.Module],
-        weights: List[float] = None,
-        p: float = 1.0,
-    ):
-
-        if weights:
-            if len(transforms) != len(weights):
-                raise ValueError(
-                    "The number of weights must match the number of transforms"
-                )
-            if not all([w >= 0 for w in weights]):
-                raise ValueError("Weights must be non-negative")
-            total_weight = sum(weights)
-            if total_weight == 0:
-                raise ValueError("At least one weight must be greater than 0")
-            self.weights = [w / total_weight for w in weights]
-        else:
-            self.weights = [1.0 / len(transforms)] * len(transforms)
-
-        if p is not None and (p < 0 or p > 1):
-            raise ValueError("p must be a float between 0 and 1")
-
-        self.transforms = transforms
-        self.p = p
-
-    def __call__(self, img):
-        if random.random() < self.p:
-            idx = random.choices(range(len(self.transforms)), weights=self.weights)[0]
-            img = self.transforms[idx](img)
-
-        return img
-
-
-class DPWTransform:
-    """
-    Transforms a wafer map to a lower DPW version. Scale param is first
-    generated using a power law distribution, then randomization is applied
-    by using it as the lower bound of a beta distribution (skewed towards the lower bound).
-
-    Parameters
-    ----------
-    domain_lower : int, optional
-        Lower bound of shapeMaxDim in the dataset, by default 26
-    domain_upper : int, optional
-        Upper bound of shapeMaxDim in the dataset, by default 212
-    out_lower : float, optional
-        Lower bound of scale param, by default 0.4
-    out_upper : float, optional
-        Upper bound of scale param, by default 0.95
-    alpha : float, optional
-        Alpha param for beta distribution, by default 0.5
-    beta : float, optional
-        Beta param for beta distribution, by default 1.0
-    p : float, optional
-        Power param for power law distribution, by default 5.0
-    """
-
-    def __init__(
-        self,
-        domain_lower: int = 26,
-        domain_upper: int = 212,
-        out_lower: float = 0.4,
-        out_upper: float = 0.95,
-        alpha: float = 0.5,
-        beta: float = 1.0,
-        p: float = 5.0,
-    ):
-
-        self.domain_lower = domain_lower
-        self.domain_upper = domain_upper
-        self.out_lower = out_lower
-        self.out_upper = out_upper
-        self.alpha = alpha
-        self.beta = beta
-        self.p = p
-
-    def power_law_transform(
-        x,
-        domain_lower,
-        domain_upper,
-        out_lower=0.4,
-        out_upper=0.95,
-        p=5,
-    ):
-        # Handle edge cases
-        if x <= domain_lower:
-            return out_upper
-        if x >= domain_upper:
-            return out_lower
-
-        # Invert input domain
-        domain_range = domain_upper - domain_lower
-        inverted_x = abs(x - domain_lower)
-        normalized_x = inverted_x / domain_range
-        # Apply power law transformation
-        y = (1 - normalized_x) ** p
-        # Map result to output range
-        out_range = out_upper - out_lower
-        return out_lower + y * out_range
-
-    def generate_skewed_random(lower_bound, upper_bound=0.95, alpha=0.5, beta=1):
-        # Generate random number using beta distribution
-        x = np.random.beta(alpha, beta)
-        # Scale to range [lower_bound, upper_bound]
-        return lower_bound + (upper_bound - lower_bound) * x
-
-    def dpw_transform(
-        wafermap,
-        scale: float,
-    ):
-        """
-        Transforms a wafer map to a lower DPW version.
-        Works by taking the central coordinates of passing and failing die, then
-        mapping those coordinates to the new wafer map.
-
-        Parameters
-        ----------
-        wafermap : np.ndarray
-            Wafermap to transform
-        scale : float, optional
-            Scale to transform the wafer map to.
-            Must be between 0 and 1.
-        """
-        assert 0.0 < scale <= 1.0, "Scale must be between 0 and 1."
-
-        # Calculate the new dimensions of the wafer after scaling down
-        h, w = wafermap.shape
-        new_h = int(h * scale)
-        new_w = int(w * scale)
-        new_dim = (new_h, new_w)
-
-        # Find the indices of the passing elements in the original wafer
-        passing_indices = np.argwhere(wafermap == 128)
-
-        # Find the indices of the failing elements in the original wafer
-        failing_indices = np.argwhere(wafermap == 255)
-
-        # Calculate the relative central coordinate of the passing and failing elements in the original wafer
-        pass_coords = (passing_indices + 0.5) / wafermap.shape
-        fail_coords = (failing_indices + 0.5) / wafermap.shape
-
-        # Calculate the central coordinates of the passing and failing elements in the new wafer map
-        new_pass_coords = (pass_coords * new_dim).astype(int)
-        new_fail_coords = (fail_coords * new_dim).astype(int)
-
-        # Create the (new_h, new_w) wafer map
-        new_wafer = np.zeros(new_dim, dtype=int)
-
-        # Assign the passing elements in the new wafer map
-        new_wafer[new_pass_coords[:, 0], new_pass_coords[:, 1]] = 128
-
-        # Assign the failing elements in the new wafer map
-        new_wafer[new_fail_coords[:, 0], new_fail_coords[:, 1]] = 255
-
-        return new_wafer
-
-    def __call__(self, img):
-        # Calculate initial scale parameter using power_law_transform
-        max_dim = max(img.shape)
-        scale_init = DPWTransform.power_law_transform(
-            max_dim,
-            self.domain_lower,
-            self.domain_upper,
-            self.out_lower,
-            self.out_upper,
-            self.p,
-        )
-
-        # Generate a skewed random scale parameter
-        lower_bound = scale_init
-        scale = DPWTransform.generate_skewed_random(
-            lower_bound, self.out_upper, alpha=self.alpha, beta=self.beta
-        )
-
-        # Apply the DPW transform using the scale parameter
-        new_img = DPWTransform.dpw_transform(img, scale=scale)
-
-        return new_img
 
 
 # def dpw_transform(wafermap, scale: float = 0.75, plot: bool = False):
